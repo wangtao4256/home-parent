8f244131cff3ac91b127c647039259632ac51a46
wangtao <wangtao_4256@163.com>
2020-07-28 10:38:43 +0800

    添加代码

--- home-quartz/src/main/java/com/home/wangtao/homequartz/controller/HomePageController.java
+++ home-quartz/src/main/java/com/home/wangtao/homequartz/controller/HomePageController.java
@@ -1,4 +1,13 @@
 package com.home.wangtao.homequartz.controller;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomePageController {
+    @GetMapping("/home")
+    public String homePage() {
+        return "home";
+    }
 }
