ac51ee26fb017d2eea873b7152b70b788cd7bf3f
wangtao <wangtao_4256@163.com>
2020-07-28 10:45:03 +0800

    添加代码

--- home-user/src/main/java/com/home/wangtao/homeuser/quartz/QuartzController.java
+++ home-user/src/main/java/com/home/wangtao/homeuser/quartz/QuartzController.java
@@ -1,6 +1,5 @@
 package com.home.wangtao.homeuser.quartz;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
