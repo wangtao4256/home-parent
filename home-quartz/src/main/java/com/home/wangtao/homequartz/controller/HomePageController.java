007c83a29038b1f2f202b92eb84d8dca5a368f09
wangtao <wangtao_4256@163.com>
2020-07-28 10:40:11 +0800

    添加代码

--- home-quartz/src/main/java/com/home/wangtao/homequartz/controller/HomePageController.java
+++ home-quartz/src/main/java/com/home/wangtao/homequartz/controller/HomePageController.java
@@ -1,6 +1,5 @@
 package com.home.wangtao.homequartz.controller;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
--- home-user/src/main/java/com/home/wangtao/homeuser/quartz/QuartzController.java
+++ home-user/src/main/java/com/home/wangtao/homeuser/quartz/QuartzController.java
@@ -1,17 +1,9 @@
 package com.home.wangtao.homeuser.quartz;
 
-import com.home.wangtao.homequartz.entity.TestApi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QuartzController {
-    @Autowired
-    private TestApi testApi;
 
-    @GetMapping("asdf")
-    public void test() {
-        testApi.test();
-    }
 }
